94d593f3592a5af57788f463c89e3dfeb18ff2bb
khatatong308 <khatatong47949@gmail.com>
2023-10-13 21:43:36 +0700

    v.1

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+//import './style.css'
+import App from './App.vue'
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+const app = createApp(App)
+app.mount('#app')
